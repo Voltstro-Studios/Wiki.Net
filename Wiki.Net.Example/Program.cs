baedb65c4d9e29723aa1eda4a2d9527297951f33
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2019-10-20 07:43:26 +1000

    Initial commit

---
+++ Wiki.Net.Example/Program.cs
@@ -0,0 +1,53 @@
+﻿#define USE_PROXY
+
+#region
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+#endregion
+
+
+namespace CreepysinStudios.WikiDotNet.Example
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			string searchString = AskUserString("Enter a search query");
+
+			bool stripHtml = AskUserBool("Strip HTML tags?");
+
+			WikiSearchResponse response = WikiSearcher.Request(searchString, stripHtml);
+
+			File.WriteAllText("./Output.txt", $"{JsonConvert.SerializeObject(response, Formatting.Indented)}");
+
+			Console.WriteLine($"\nResults found ({searchString}):\n");
+			for (int i = 0; i < response.Query.SearchResults.Length; i++)
+			{
+				WikiSearchResult result = response.Query.SearchResults[i];
+				Console.WriteLine($"\t[Page {result.PageId}]\t{result.Title} ({result.WordCount} words, {result.Size} bytes):\t{result.Preview}...\n\tAt {result.Url}\n");
+			}
+
+			Console.ReadLine();
+		}
+
+		private static bool AskUserBool(string message, bool clearConsole = true)
+		{
+			if (clearConsole)
+				Console.Clear();
+			Console.WriteLine(message);
+			// ReSharper disable once AssignNullToNotNullAttribute
+			return bool.Parse(Console.ReadLine());
+		}
+
+		private static string AskUserString(string message, bool clearConsole = true)
+		{
+			if (clearConsole)
+				Console.Clear();
+			Console.WriteLine(message);
+			return Console.ReadLine();
+		}
+	}
+}
---
+++ Wiki.Net/WikiSearchResponse.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.Net.Http;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace CreepysinStudios.WikiDotNet
+{
+	// ReSharper disable once ClassNeverInstantiated.Global
+	public sealed class WikiSearchResponse
+	{
+		// ReSharper disable once UnassignedField.Global
+		public WikiQuery Query;
+
+		// ReSharper disable once UnassignedField.Global
+		public HttpResponseMessage ResponseMessage;
+
+		public sealed class WikiQuery
+		{
+			[JsonProperty("search")] public WikiSearchResult[] SearchResults;
+		}
+	}
+}
