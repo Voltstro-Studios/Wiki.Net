5b2f868c03dec1d30499face56429a386fe91937
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2019-10-20 12:09:00 +1000

    Upgrade tag stripper for HTML results

--- Wiki.Net.Example/Example.cs
+++ Wiki.Net.Example/Example.cs
@@ -13,10 +13,8 @@ namespace CreepysinStudios.WikiDotNet.Example
 		private static void Main()
 		{
 			string searchString = AskUserString("Enter a search query");
-
-			bool stripHtml = AskUserBool("Strip HTML tags?");
-
-			WikiSearchResponse response = WikiSearcher.Request(searchString, stripHtml);
+			
+			WikiSearchResponse response = WikiSearcher.Request(searchString);
 
 			File.WriteAllText("./Output.txt", $"{JsonConvert.SerializeObject(response, Formatting.Indented)}");
 
@@ -28,15 +26,6 @@ namespace CreepysinStudios.WikiDotNet.Example
 			}
 
 			Console.ReadLine();
-		}
-
-		private static bool AskUserBool(string message, bool clearConsole = true)
-		{
-			if (clearConsole)
-				Console.Clear();
-			Console.WriteLine(message);
-			// ReSharper disable once AssignNullToNotNullAttribute
-			return bool.Parse(Console.ReadLine());
 		}
 
 		private static string AskUserString(string message, bool clearConsole = true)
