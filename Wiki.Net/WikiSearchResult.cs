5f9340e1ef73a74784ccacfd9d9f10cb4d3d87b2
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2019-10-22 16:48:44 +1000

    Make Search results readonly

--- Wiki.Net/WikiSearchResult.cs
+++ Wiki.Net/WikiSearchResult.cs
@@ -10,7 +10,7 @@ namespace CreepysinStudios.WikiDotNet
 	/// <summary>
 	///     A single search result from a wikipedia search
 	/// </summary>
-	
+
 	//Todo Add what categories the article falls into
 	//!Make things readonly if possible
 	public sealed class WikiSearchResult
@@ -18,36 +18,36 @@ namespace CreepysinStudios.WikiDotNet
 		/// <summary>
 		///     The last time this page was edited
 		/// </summary>
-		[JsonProperty("timestamp")] public DateTime LastEdited;
+		[JsonProperty("timestamp")] public readonly DateTime LastEdited;
 
 		/// <summary>
 		///     The numerical ID that corresponds internally (in Wikipedia's servers) to this page
 		/// </summary>
-		[JsonProperty("pageid")] public int PageId;
+		[JsonProperty("pageid")] public readonly int PageId;
 
 		/// <summary>
 		///     A preview of the page
 		/// </summary>
-		[JsonProperty("snippet")] public string Preview;
+		[JsonProperty("snippet")] public readonly string Preview;
 
 		/// <summary>
 		///     (Possibly) How large the entire page is (assumed in bytes). Unknown what this actually is/does.
 		/// </summary>
-		[JsonProperty("size")] public int Size;
+		[JsonProperty("size")] public readonly int Size;
 
 		/// <summary>
 		///     The title of this page
 		/// </summary>
-		[JsonProperty("title")] public string Title;
+		[JsonProperty("title")] public readonly string Title;
+
+		/// <summary>
+		///     How many words are in the article
+		/// </summary>
+		[JsonProperty("wordcount")] public readonly int WordCount;
 
 		/// <summary>
 		///     The URL that can be used to access the article online. Created using the Page ID
 		/// </summary>
 		public string Url => $"https://en.wikipedia.org/?curid={PageId}";
-
-		/// <summary>
-		///     How many words are in the article
-		/// </summary>
-		[JsonProperty("wordcount")] public int WordCount;
 	}
 }
