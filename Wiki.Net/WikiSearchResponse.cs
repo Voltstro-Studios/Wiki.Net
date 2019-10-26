e5108205fa34040d6751064b41a30b3be1a53fcc
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2019-10-27 06:57:27 +1000

    Remove unnecessary fields from WikiSearchResponse

--- Wiki.Net/WikiSearchResponse.cs
+++ Wiki.Net/WikiSearchResponse.cs
@@ -1,7 +1,6 @@
 ﻿#region
 
 using System;
-using System.Net.Http;
 
 #endregion
 
@@ -13,32 +12,17 @@ namespace CreepysinStudios.WikiDotNet
 	public sealed class WikiSearchResponse
 	{
 		/// <summary>
-		///     The Json string from which the results were taken
-		/// </summary>
-		public readonly string JsonResult;
-
-		/// <summary>
-		///     The response message from which the <see cref="SearchResults" /> and <see cref="JsonResult" /> are parsed
-		/// </summary>
-		public readonly HttpResponseMessage ResponseMessage;
-
-		/// <summary>
 		///     An array of results returned from the wikipedia servers
 		/// </summary>
 		public readonly WikiSearchResult[] SearchResults;
-
+		
 		/// <summary>
 		///     A constructor that creates a new <see cref="WikiSearchResponse" />
 		/// </summary>
-		/// <param name="jsonResult">The Json string used to parse the search results</param>
-		/// <param name="responseMessage">The <see cref="HttpResponseMessage" /> that was returned from the server</param>
 		/// <param name="searchResults">An array of parsed search results</param>
-		internal WikiSearchResponse(string jsonResult,
-			HttpResponseMessage responseMessage, WikiSearchResult[] searchResults)
+		internal WikiSearchResponse(WikiSearchResult[] searchResults)
 		{
-			JsonResult = jsonResult ?? throw new ArgumentNullException(nameof(jsonResult));
 			SearchResults = searchResults ?? throw new ArgumentNullException(nameof(searchResults));
-			ResponseMessage = responseMessage ?? throw new ArgumentNullException(nameof(responseMessage));
 		}
 	}
 }
